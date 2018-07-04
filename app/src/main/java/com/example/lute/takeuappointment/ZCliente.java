72150d453752e3592597262336a338a4e020eeb5
luffyxiao <luisjesus.delgado89@gmail.com>
2018-07-04 16:19:50 +0200

    clases cliente y profesonal con contrucstor, getter y setter

---
+++ app/src/main/java/com/example/lute/takeuappointment/ZCliente.java
@@ -0,0 +1,198 @@
+package com.example.lute.takeuappointment;
+
+public class ZCliente {
+
+    String cusuario;
+    String ccontrasena;
+    String cnombre;
+    String capellido;
+    String cfecha;
+    String chora;
+    String clugar;
+    String cdireccion;
+    String cprofesional;
+    String c1;
+    String c2;
+    String c3;
+    String c4;
+    String c5;
+    String c6;
+    String c7;
+    String c8;
+    String c9;
+    String c10;
+
+    public ZCliente(String cusuario, String ccontrasena, String cnombre, String capellido, String cfecha, String chora, String clugar, String cdireccion, String cprofesional, String c1, String c2, String c3, String c4, String c5, String c6, String c7, String c8, String c9, String c10) {
+        this.cusuario = cusuario;
+        this.ccontrasena = ccontrasena;
+        this.cnombre = cnombre;
+        this.capellido = capellido;
+        this.cfecha = cfecha;
+        this.chora = chora;
+        this.clugar = clugar;
+        this.cdireccion = cdireccion;
+        this.cprofesional = cprofesional;
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+        this.c4 = c4;
+        this.c5 = c5;
+        this.c6 = c6;
+        this.c7 = c7;
+        this.c8 = c8;
+        this.c9 = c9;
+        this.c10 = c10;
+    }
+
+    public String getCusuario() {
+        return cusuario;
+    }
+
+    public void setCusuario(String cusuario) {
+        this.cusuario = cusuario;
+    }
+
+    public String getCcontrasena() {
+        return ccontrasena;
+    }
+
+    public void setCcontrasena(String ccontrasena) {
+        this.ccontrasena = ccontrasena;
+    }
+
+    public String getCnombre() {
+        return cnombre;
+    }
+
+    public void setCnombre(String cnombre) {
+        this.cnombre = cnombre;
+    }
+
+    public String getCapellido() {
+        return capellido;
+    }
+
+    public void setCapellido(String capellido) {
+        this.capellido = capellido;
+    }
+
+    public String getCfecha() {
+        return cfecha;
+    }
+
+    public void setCfecha(String cfecha) {
+        this.cfecha = cfecha;
+    }
+
+    public String getChora() {
+        return chora;
+    }
+
+    public void setChora(String chora) {
+        this.chora = chora;
+    }
+
+    public String getClugar() {
+        return clugar;
+    }
+
+    public void setClugar(String clugar) {
+        this.clugar = clugar;
+    }
+
+    public String getCdireccion() {
+        return cdireccion;
+    }
+
+    public void setCdireccion(String cdireccion) {
+        this.cdireccion = cdireccion;
+    }
+
+    public String getCprofesional() {
+        return cprofesional;
+    }
+
+    public void setCprofesional(String cprofesional) {
+        this.cprofesional = cprofesional;
+    }
+
+    public String getC1() {
+        return c1;
+    }
+
+    public void setC1(String c1) {
+        this.c1 = c1;
+    }
+
+    public String getC2() {
+        return c2;
+    }
+
+    public void setC2(String c2) {
+        this.c2 = c2;
+    }
+
+    public String getC3() {
+        return c3;
+    }
+
+    public void setC3(String c3) {
+        this.c3 = c3;
+    }
+
+    public String getC4() {
+        return c4;
+    }
+
+    public void setC4(String c4) {
+        this.c4 = c4;
+    }
+
+    public String getC5() {
+        return c5;
+    }
+
+    public void setC5(String c5) {
+        this.c5 = c5;
+    }
+
+    public String getC6() {
+        return c6;
+    }
+
+    public void setC6(String c6) {
+        this.c6 = c6;
+    }
+
+    public String getC7() {
+        return c7;
+    }
+
+    public void setC7(String c7) {
+        this.c7 = c7;
+    }
+
+    public String getC8() {
+        return c8;
+    }
+
+    public void setC8(String c8) {
+        this.c8 = c8;
+    }
+
+    public String getC9() {
+        return c9;
+    }
+
+    public void setC9(String c9) {
+        this.c9 = c9;
+    }
+
+    public String getC10() {
+        return c10;
+    }
+
+    public void setC10(String c10) {
+        this.c10 = c10;
+    }
+}
---
+++ app/src/main/java/com/example/lute/takeuappointment/ZProfesional.java
@@ -0,0 +1,168 @@
+package com.example.lute.takeuappointment;
+
+public class ZProfesional {
+
+    String pusuario;
+    String pcontrasena;
+    String pnombre;
+    String papellido;
+    String plugar;
+    String pdireccion;
+    String p1;
+    String p2;
+    String p3;
+    String p4;
+    String p5;
+    String p6;
+    String p7;
+    String p8;
+    String p9;
+    String p10;
+
+    public ZProfesional(String pusuario, String pcontrasena, String pnombre, String papellido, String plugar, String pdireccion, String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8, String p9, String p10) {
+        this.pusuario = pusuario;
+        this.pcontrasena = pcontrasena;
+        this.pnombre = pnombre;
+        this.papellido = papellido;
+        this.plugar = plugar;
+        this.pdireccion = pdireccion;
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+        this.p5 = p5;
+        this.p6 = p6;
+        this.p7 = p7;
+        this.p8 = p8;
+        this.p9 = p9;
+        this.p10 = p10;
+    }
+
+    public String getPusuario() {
+        return pusuario;
+    }
+
+    public void setPusuario(String pusuario) {
+        this.pusuario = pusuario;
+    }
+
+    public String getPcontrasena() {
+        return pcontrasena;
+    }
+
+    public void setPcontrasena(String pcontrasena) {
+        this.pcontrasena = pcontrasena;
+    }
+
+    public String getPnombre() {
+        return pnombre;
+    }
+
+    public void setPnombre(String pnombre) {
+        this.pnombre = pnombre;
+    }
+
+    public String getPapellido() {
+        return papellido;
+    }
+
+    public void setPapellido(String papellido) {
+        this.papellido = papellido;
+    }
+
+    public String getPlugar() {
+        return plugar;
+    }
+
+    public void setPlugar(String plugar) {
+        this.plugar = plugar;
+    }
+
+    public String getPdireccion() {
+        return pdireccion;
+    }
+
+    public void setPdireccion(String pdireccion) {
+        this.pdireccion = pdireccion;
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+    public String getP2() {
+        return p2;
+    }
+
+    public void setP2(String p2) {
+        this.p2 = p2;
+    }
+
+    public String getP3() {
+        return p3;
+    }
+
+    public void setP3(String p3) {
+        this.p3 = p3;
+    }
+
+    public String getP4() {
+        return p4;
+    }
+
+    public void setP4(String p4) {
+        this.p4 = p4;
+    }
+
+    public String getP5() {
+        return p5;
+    }
+
+    public void setP5(String p5) {
+        this.p5 = p5;
+    }
+
+    public String getP6() {
+        return p6;
+    }
+
+    public void setP6(String p6) {
+        this.p6 = p6;
+    }
+
+    public String getP7() {
+        return p7;
+    }
+
+    public void setP7(String p7) {
+        this.p7 = p7;
+    }
+
+    public String getP8() {
+        return p8;
+    }
+
+    public void setP8(String p8) {
+        this.p8 = p8;
+    }
+
+    public String getP9() {
+        return p9;
+    }
+
+    public void setP9(String p9) {
+        this.p9 = p9;
+    }
+
+    public String getP10() {
+        return p10;
+    }
+
+    public void setP10(String p10) {
+        this.p10 = p10;
+    }
+}
