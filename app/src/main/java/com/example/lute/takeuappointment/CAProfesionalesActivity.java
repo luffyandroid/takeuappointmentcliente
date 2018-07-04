96a6673941ecc0c069898878190e8e22c659574b
melania <melania.gallego.22@gmail.com>
2018-07-04 12:26:02 +0200

    Act Profesionales

---
+++ app/src/main/java/com/example/lute/takeuappointment/CAProfesionalesActivity.java
@@ -0,0 +1,13 @@
+package com.example.lute.takeuappointment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CAProfesionalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caprofesionales);
+    }
+}
